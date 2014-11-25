24f595e1205e3e5cd02b1e312f90bb9ec8514288
Michal Bojanowski <michal2992@gmail.com>
2014-11-25 10:22:45 +0100

    Updated Refugees docs and roxygenization

--- R/Refugees.R
+++ R/Refugees.R
@@ -1,10 +1,13 @@
 #' Refugees data
 #'
-#' Top 10 countries/territories of origin* for period 2003-13 of UNHCR statistics on "Persons recognized as refugees under the 1951 UN Convention/1967 Protocol, the 1969 OAU Convention, in accordance with the UNHCR Statute, persons granted a complementary form of protection and those granted temporary protection."
-#' *  excluding the "Various" category
+#' Top 10 countries/territories of origin (excluding "Various") for period
+#' 2003-13 of UNHCR statistics on "Persons recognized as refugees under the
+#' 1951 UN Convention/1967 Protocol, the 1969 OAU Convention, in accordance
+#' with the UNHCR Statute, persons granted a complementary form of protection
+#' and those granted temporary protection."
 #'
 #' @format
-#' Describe columns
+#' Data frame with the following columns:
 #' \describe{
 #' \item{country}{Country or territory of origin}
 #' \item{year}{Year (2003-13)}
@@ -14,8 +17,6 @@
 #' @source
 #' http://data.un.org/Data.aspx?d=UNHCR&f=indID%3aType-Ref
 #'
-#' @references
-#' 
 #' @docType data
 #' @name Refugees
 NULL
