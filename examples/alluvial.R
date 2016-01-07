3854c994fad50b6cc9e776816111d82d5a08c0b5
Cory Brunson <brunson@uchc.edu>
2016-01-07 09:40:35 -0500

    merge examples and document()

--- examples/alluvial.R
+++ examples/alluvial.R
@@ -27,6 +27,18 @@ alluvial( tit[,1:4], freq=tit$Freq, border=NA,
          col=ifelse( tit$Class == "3rd" & tit$Sex == "Male", "red", "gray") )
 
 
+# Possible blocks options
+for (blocks in c(TRUE, FALSE, "bookends")) {
+    
+    # Elaborate alluvial diagram from main examples file
+    alluvial( tit[, 1:4], freq = tit$Freq, border = NA,
+              hide = tit$Freq < quantile(tit$Freq, .50),
+              col = ifelse( tit$Class == "3rd" & tit$Sex == "Male",
+                            "red", "gray" ),
+              blocks = blocks )
+}
+
+
 ### POLPAN data
 if(FALSE)
 {
