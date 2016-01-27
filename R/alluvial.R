dcf99ca5aecc3182cb7bab63ff6fc122233772cf
Cory Brunson <brunson@uchc.edu>
2016-01-27 12:53:47 -0500

    use 'hide' column in p rather than raw input to exclude ribbons

--- R/alluvial.R
+++ R/alluvial.R
@@ -91,7 +91,7 @@ alluvial <- function( ..., freq,
   plot(NULL, type="n", xlim=c(1-cw, np+cw), ylim=c(0, 1), xaxt="n", yaxt="n",
        xaxs="i", yaxs="i", xlab='', ylab='', frame=FALSE)
   # For every stripe
-  ind <- rev(order(p$layer)[!hide])
+  ind <- rev(order(p[!hide, ]$layer))
   for(i in ind )
   {
     # For every inter-dimensional segment
