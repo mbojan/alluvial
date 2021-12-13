d21a9671de88dbba6687810897fdadee18b084c5
Michal Bojanowski <michal2992@gmail.com>
2021-12-13 19:25:40 +0100

    Reformat the test

--- tests/testthat/test-alluvial.R
+++ tests/testthat/test-alluvial.R
@@ -1,11 +1,11 @@
-context("Oddities in input data")
+# Oddities in input data --------------------------------------------------
 
-test_that("Character vectors in data do not trigger warnings",
-          {
-            d <- data.frame(x1=rep(letters[1:2], 2),
-                            x2=rep(letters[1:2], each=2),
-                            freq=1:4, 
-                            stringsAsFactors=FALSE)
-            expect_silent( alluvial(d[,1:2], freq=d$freq) )
-          } )
+test_that("Character vectors in data do not trigger warnings", {
+  d <- data.frame(
+    x1=rep(letters[1:2], 2),
+    x2=rep(letters[1:2], each=2),
+    freq=1:4
+  )
+  expect_silent( alluvial(d[,1:2], freq=d$freq) )
+})
 
