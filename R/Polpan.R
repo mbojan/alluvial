9221fea474f8f92cf7e42487068eb27d58a78e3b
Michał Bojanowski <michal2992@gmail.com>
2026-05-04 14:53:05 +0200

    Add POLPAN voting data
    Thanks to @mkolczynska

---
+++ R/Polpan.R
@@ -0,0 +1,18 @@
+#' POLPAN voting flows
+#' 
+#' Subset of POLPAN panel study.
+#'
+#' @format
+#' Data frame with the following columns:
+#' \describe{
+#' \item{party2007}{Voting in 2007}
+#' \item{party2011}{Voting in 2011}
+#' \item{party2015}{Voting in 2015}
+#' \item{n}{Counts}
+#' }
+#'
+#' @source
+#' \url{https://www.polpan.org}
+#'
+#' @docType data
+"Polpan"
